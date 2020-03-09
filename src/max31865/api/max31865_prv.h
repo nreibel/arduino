d43ce99eb893fa9072270bc1b02321bde012dea8
Nicolas Reibel <nre@2crsi.com>
2020-03-09 18:03:50 +0100

    Added support for max31865 PT100 controller

---
+++ src/max31865/api/max31865_prv.h
@@ -0,0 +1,19 @@
+#ifndef SRC_MAX31865_API_MAX31865_PRV_H_
+#define SRC_MAX31865_API_MAX31865_PRV_H_
+
+#include "max31865.h"
+#include "max31865_cfg.h"
+
+#define MAX31865_REG_CONFIG       0x00
+#define MAX31865_REG_RTD_MSB      0x01
+#define MAX31865_REG_RTD_LSB      0x02
+#define MAX31865_REG_HFT_MSB      0x03
+#define MAX31865_REG_HFT_LSB      0x04
+#define MAX31865_REG_LFT_MSB      0x05
+#define MAX31865_REG_LFT_LSB      0x06
+#define MAX31865_REG_FAULT_STATUS 0x07
+
+#define MAX31865_READ  0x00
+#define MAX31865_WRITE 0x80
+
+#endif // SRC_MAX31865_API_MAX31865_PRV_H_
---
+++ src/max31865/src/max31865.c
@@ -0,0 +1,60 @@
+#include "max31865.h"
+#include "max31865_prv.h"
+#include "max31865_cfg.h"
+#include "spi.h"
+#include "bits.h"
+#include "types.h"
+
+#include <math.h>
+
+#include <stdio.h>
+#include "serial.h"
+
+void MAX31865_Init()
+{
+    // VBIAS = On, Conversion mode = Auto, 3-wire RTD
+    byte configuration = BIT(7) | BIT(6) | BIT(4);
+
+    Spi_WriteByte(MAX31865_REG_CONFIG | MAX31865_WRITE, NULL_PTR);
+    Spi_WriteByte(configuration, NULL_PTR);
+}
+
+// Implementation of Callendar-Van Dusen equation
+// Only for temperatures > 0°C
+double MAX31865_RTD_To_Temperature(double rtd)
+{
+    double acc = 0;
+    acc = rtd/MAX31865_PT_RES;
+    acc = 1 - acc;
+    acc = 4 * MAX31865_B * acc;
+    acc = (MAX31865_A * MAX31865_A) - acc;
+    acc = sqrt(acc) - MAX31865_A;
+    return acc / (2 * MAX31865_B);
+}
+
+Std_ReturnType MAX31865_ReadRTD(double *rtd)
+{
+    Std_ReturnType retval = Status_Not_OK;
+
+    byte msb = 0;
+    byte lsb = 0;
+
+    Spi_WriteByte(MAX31865_REG_RTD_MSB | MAX31865_READ, NULL_PTR);
+    Spi_WriteByte(0, &msb);
+    Spi_WriteByte(0, &lsb);
+
+    if (IS_SET_BIT(lsb, 0))
+    {
+        // TODO : handle fault bit
+    }
+    else
+    {
+        word w = (msb << 8) | lsb;
+        *rtd = (MAX31865_REF * TYPECAST(w, double)) / 0x7FFFULL;
+        retval = Status_OK;
+    }
+
+    return retval;
+}
+
+// double MAX31865_
