8d5176c6daefc96c9145a446eb5b69a37bdc8741
nreibel <nicolas.reibel@gmail.com>
2020-10-20 10:15:23 +0200

    Move App_Init prototype declaration to OS

--- src/os/api/os.h
+++ src/os/api/os.h
@@ -9,12 +9,11 @@ typedef Std_ReturnType (*BackgroundTask)(void);
 void   Os_ResetTimer(Timer timer);
 time_t Os_GetTimerValue(Timer timer);
 void   Os_SetupTask(Timer timer, time_t interval, Callback callback, void* param);
-
 void   Os_Wait(time_t ms);
+void   Os_EnableInterrupts();
+void   Os_DisableInterrupts();
+void   Os_HardReset();
 
-void Os_EnableInterrupts();
-void Os_DisableInterrupts();
-
-void Os_HardReset();
+extern void App_Init(void);
 
 #endif /* OS_API_OS_H_ */
