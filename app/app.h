8b7a9c1f84786590eaa709534d35cd9c82832e1d
nreibel <nicolas.reibel@gmail.com>
2021-01-28 12:30:10 +0100

    Restore minimal dummy app

--- app/app.h
+++ app/app.h
@@ -2,7 +2,5 @@
 #define SRC_APP_API_APP_H_
 
 Std_ReturnType Task_MainCyclic(void* data);
-Std_ReturnType Task_Accelerometer(void* data);
-Std_ReturnType Task_LedMatrix(void* data);
 
 #endif /* SRC_APP_API_APP_H_ */
