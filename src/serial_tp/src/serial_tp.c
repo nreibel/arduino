e275d59ee3e617b187376bf28f392641b32ca00b
nreibel <nicolas.reibel@gmail.com>
2021-02-04 16:53:06 +0100

    [SERIAL_TP] Reserve some memory for serial_tp response buffer

--- src/serial_tp/src/serial_tp.c
+++ src/serial_tp/src/serial_tp.c
@@ -1,6 +1,9 @@
 #include "serial_tp.h"
+#include "serial_tp_cfg.h"
 #include "types.h"
 #include "bits.h"
+
+static uint8_t buffer[SERIAL_TP_RESPONSE_BUFFER_LEN] = {0};
 
 void Serial_TP_Init()
 {
@@ -10,7 +13,7 @@ void Serial_TP_Init()
 void Serial_RxCallback(const char *rx_buffer, int length)
 {
     Serial_TP_Request *req = TYPECAST(rx_buffer, Serial_TP_Request*);
-    Serial_TP_Response rsp = {SERIAL_TP_RETCODE_ERROR, 0, NULL_PTR};
+    Serial_TP_Response rsp = {SERIAL_TP_RETCODE_ERROR, 0, buffer};
 
     if ( req->header != SERIAL_TP_REQUEST_HEADER )
     {
