fcd93d1f005141113da2fcc68e730d0a375e9ae5
nre <nre@2crsi.com>
2021-06-11 14:59:43 +0200

    [GPIO] Add PCINT (Port Change Interrupts) [ST7735] Fix code

--- src/gpio/api/gpio.h
+++ src/gpio/api/gpio.h
@@ -24,7 +24,14 @@ typedef enum
     GPIO_PORT_B,
     GPIO_PORT_C,
     GPIO_PORT_D,
+    NBR_OF_PORTS
 } gpio_port_t;
+
+typedef enum {
+    GPIO_INT_0,
+    GPIO_INT_1,
+    NBR_OF_EXTINT
+} gpio_int_t;
 
 typedef struct {
     gpio_port_t port;
@@ -32,12 +39,14 @@ typedef struct {
     gpio_data_direction_t direction;
 } gpio_t;
 
-typedef void (*gpio_interrupt_callback_t)(volatile void*);
+typedef void (*gpio_extint_cbk_t)(volatile void*);
+typedef void (*gpio_pcint_cbk_t)(uint8_t mask, volatile void*);
 
 int gpio_init(gpio_t *self, gpio_port_t port, uint8_t pin, gpio_data_direction_t direction);
 int gpio_set(gpio_t *self);
 int gpio_reset(gpio_t *self);
 int gpio_get(gpio_t *self, bool *state);
-int gpio_enable_interrupt(gpio_t *self, gpio_edge_t edge, gpio_interrupt_callback_t cbk, volatile void *data);
+int gpio_enable_extint(gpio_int_t pin, gpio_edge_t edge, gpio_extint_cbk_t cbk, volatile void *data);
+int gpio_enable_pcint(gpio_port_t port, uint8_t mask, gpio_pcint_cbk_t cbk, volatile void *data);
 
 #endif /* __GPIO_H__ */
