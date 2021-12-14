df7ed23e70a899d31c925585bd66e5b58f864b92
nre <nre@2crsi.com>
2021-12-14 10:27:53 +0100

    [SERIAL_TP] Additional error checking

--- src/serial_tp/libs/python3/serialtp.py
+++ src/serial_tp/libs/python3/serialtp.py
@@ -5,6 +5,7 @@ import struct
 from serial import Serial, SerialException
 
 RETCODE_OK       = 0x00
+FRAME_HEADER     = 0xA0
 FRAME_TERMINATOR = b'\x0D'
 
 class SerialTPException(Exception):
@@ -29,9 +30,8 @@ class SerialTPException(Exception):
 
 class SerialTP:
 
-
     def __init__(self, tty: str, baud: int):
-        self._ser = Serial(tty, baud)
+        self._ser = Serial(tty, baud) # TODO timeout=5
         data = self._ser.read(5)
 
         if data.decode('utf8') != 'READY':
@@ -46,7 +46,7 @@ class SerialTP:
     def send_command(self, fn, addr = 0, data = None):
         # Send request header
         length = 0 if data == None else len(data)
-        cmd = struct.pack('BBBB', 0xA0, fn, addr, length)
+        cmd = struct.pack('BBBB', FRAME_HEADER, fn, addr, length)
         self._ser.write(cmd)
 
         # Send request data
@@ -59,20 +59,18 @@ class SerialTP:
         # Processing on client side...
 
         # Read response header
-        # TODO : read until timeout
         rsp = self._ser.read(2)
-        (status, data_len) = struct.unpack('BB', rsp)
+        (status, length) = struct.unpack('BB', rsp)
 
         # Read response data
         data = None
-        if data_len > 0:
-            data = self._ser.read(data_len)
+        if length > 0:
+            data = self._ser.read(length)
 
         # Read frame terminator
         term = self._ser.read(1)
 
         if term != FRAME_TERMINATOR:
-            print('Expected EOF')
             return False, None
 
         elif status == RETCODE_OK:
@@ -81,46 +79,39 @@ class SerialTP:
         else:
             return False, status
 
+    def read_format(self, fn, addr, fmt):
+        (status, retval) = self.send_command(fn, addr)
+        if not status: raise SerialTPException(retval)
+        try: return struct.unpack(fmt, retval)
+        except struct.error: raise SerialTPException(SerialTPException.ERROR_INVALID_DATA_LEN)
 
     def write_string(self, fn, addr, string):
         data = string.encode('utf8')
         (status, retval) = self.send_command(fn, addr, data)
         if not status: raise SerialTPException(retval)
 
-
     def write_byte(self, fn, addr, byte):
         try: data = struct.pack('B', byte)
-        except struct.error: raise Exception('Format error')
+        except struct.error: raise SerialTPException(SerialTPException.ERROR_INVALID_DATA_LEN)
         (status, retval) = self.send_command(fn, addr, data)
         if not status: raise SerialTPException(retval)
-
 
     def write_word(self, fn, addr, byte):
         try: data = struct.pack('H', byte)
-        except struct.error: raise Exception('Format error')
+        except struct.error: raise SerialTPException(SerialTPException.ERROR_INVALID_DATA_LEN)
         (status, retval) = self.send_command(fn, addr, data)
         if not status: raise SerialTPException(retval)
-
 
     def read_string(self, fn, addr):
         (status, retval) = self.send_command(fn, addr)
         if status: return retval.decode('utf8')
         else: raise SerialTPException(retval)
 
-
     def read_byte(self, fn, addr):
-        (status, retval) = self.send_command(fn, addr)
-        if status: return struct.unpack('B', retval)[0]
-        else: raise SerialTPException(retval)
-
+        return self.read_format(fn, addr, 'B')[0];
 
     def read_word(self, fn, addr):
-        (status, retval) = self.send_command(fn, addr)
-        if status: return struct.unpack('H', retval)[0]
-        else: raise SerialTPException(retval)
-
+        return self.read_format(fn, addr, 'H')[0];
 
     def read_float(self, fn, addr):
-        (status, retval) = self.send_command(fn, addr)
-        if status: return struct.unpack('f', retval)[0]
-        else: raise SerialTPException(retval)
+        return self.read_format(fn, addr, 'f')[0];
