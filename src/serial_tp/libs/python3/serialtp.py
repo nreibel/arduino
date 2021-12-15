601529b11c8f935d427b2b6695c795df91bb9ccb
nre <nre@2crsi.com>
2021-12-15 16:53:59 +0100

    [SERIAL_TP] Improve error handling in python lib

--- src/serial_tp/libs/python3/serialtp.py
+++ src/serial_tp/libs/python3/serialtp.py
@@ -11,18 +11,18 @@ FRAME_TERMINATOR = b'\x0D'
 class SerialTPException(Exception):
 
     # Generic SerialTP return values
-    OK            = 0x00
     ERROR         = 0x01
     OUT_OF_BOUNDS = 0x02
 
     # SerialTP Protol errors
-    ERROR_INVALID_HEADER   = 0x10
-    ERROR_INVALID_DATA_LEN = 0x11
+    PROTOCOL         = 0x10
+    INVALID_HEADER   = 0x11
+    INVALID_DATA_LEN = 0x12
 
     # SerialTP User Callback errors
-    ERROR_FUNCTION_INVALID = 0x20
-    ERROR_ADDRESS_INVALID  = 0x21
-    ERROR_DATA_INVALID     = 0x22
+    FUNCTION_INVALID = 0x20
+    ADDRESS_INVALID  = 0x21
+    DATA_INVALID     = 0x22
 
     def __init__(self, code:int=ERROR):
         self.code = code
@@ -31,11 +31,12 @@ class SerialTPException(Exception):
 class SerialTP:
 
     def __init__(self, tty: str, baud: int):
-        self._ser = Serial(tty, baud) # TODO timeout=5
-        data = self._ser.read(5)
+        self._ser = Serial(tty, baud)
+        for i in range(5):
+            data = self._ser.read(5)
+            if data.decode('utf8') == 'READY': return;
 
-        if data.decode('utf8') != 'READY':
-            raise Exception('Wrong handshake received')
+        raise Exception('Wrong handshake received')
 
 
     def __del__(self):
@@ -70,53 +71,65 @@ class SerialTP:
         # Read frame terminator
         term = self._ser.read(1)
 
-        if term != FRAME_TERMINATOR:
-            return False, None
+        if term != FRAME_TERMINATOR: raise SerialTPException(SerialTPException.PROTOCOL)
+        elif status == RETCODE_OK: return True, data
+        else: return False, status
 
-        elif status == RETCODE_OK:
-            return True, data
 
-        else:
-            return False, status
+    def read(self, fn, addr):
+        try:
+            (status, retval) = self.send_command(fn, addr)
+            if not status: raise SerialTPException(retval)
+            else: return retval
+        except struct.error:
+            raise SerialTPException(SerialTPException.PROTOCOL)
+
+
+    def write(self, fn, addr, data):
+        try:
+            (status, retval) = self.send_command(fn, addr, data)
+            if not status: raise SerialTPException(retval)
+        except struct.error:
+            raise SerialTPException(SerialTPException.PROTOCOL)
 
     def read_format(self, fn, addr, fmt):
-        (status, retval) = self.send_command(fn, addr)
-        if not status: raise SerialTPException(retval)
-        try: return struct.unpack(fmt, retval)
-        except struct.error: raise SerialTPException(SerialTPException.ERROR_INVALID_DATA_LEN)
+        data = self.read(fn, addr)
+        try: return struct.unpack(fmt, data)
+        except struct.error: raise SerialTPException(SerialTPException.INVALID_DATA_LEN)
+
 
     def write_string(self, fn, addr, string):
         data = string.encode('utf8')
-        (status, retval) = self.send_command(fn, addr, data)
-        if not status: raise SerialTPException(retval)
+        self.write(fn, addr, data)
+
 
     def write_byte(self, fn, addr, byte):
         try: data = struct.pack('B', byte)
-        except struct.error: raise SerialTPException(SerialTPException.ERROR_INVALID_DATA_LEN)
-        else:
-            (status, retval) = self.send_command(fn, addr, data)
-            if not status: raise SerialTPException(retval)
+        except struct.error: raise SerialTPException(SerialTPException.INVALID_DATA_LEN)
+        else: self.write(fn, addr, data)
+
 
     def write_word(self, fn, addr, byte):
         try: data = struct.pack('H', byte)
-        except struct.error: raise SerialTPException(SerialTPException.ERROR_INVALID_DATA_LEN)
-        else:
-            (status, retval) = self.send_command(fn, addr, data)
-            if not status: raise SerialTPException(retval)
+        except struct.error: raise SerialTPException(SerialTPException.INVALID_DATA_LEN)
+        else: self.write(fn, addr, data)
+
 
     def read_string(self, fn, addr):
-        (status, retval) = self.send_command(fn, addr)
-        if status: return retval.decode('utf8')
-        else: raise SerialTPException(retval)
+        data = self.read(fn, addr)
+        return data.decode('utf8')
+
 
     def read_byte(self, fn, addr):
         try: return self.read_format(fn, addr, 'B')[0];
-        except IndexError: raise SerialTPException(SerialTPException.ERROR_DATA_INVALID)
+        except IndexError: raise SerialTPException(SerialTPException.DATA_INVALID)
+
 
     def read_word(self, fn, addr):
         try: return self.read_format(fn, addr, 'H')[0];
-        except IndexError: raise SerialTPException(SerialTPException.ERROR_DATA_INVALID)
+        except IndexError: raise SerialTPException(SerialTPException.DATA_INVALID)
+
 
     def read_float(self, fn, addr):
         try: return self.read_format(fn, addr, 'f')[0];
-        except IndexError: raise SerialTPException(SerialTPException.ERROR_DATA_INVALID)
+        except IndexError: raise SerialTPException(SerialTPException.DATA_INVALID)
