4cb9a415f0228942a80a616f5b933cb24c336ae4
nre <nre@2crsi.com>
2021-12-23 15:49:00 +0100

    [OS] Make sure that IS_SET_BIT returns a proper boolean value [APP] Update app

--- app/app.c
+++ app/app.c
@@ -7,13 +7,13 @@
 
 /*
  * Timer demo :
- * - Timer 0 : Output 244Hz PWM at 50% duty cycle on OC0A (D6)
+ * - Timer 0 : Output 200Hz PWM at 50% duty cycle on OC0B (D5)
  * - Timer 1 : Input Capture mode on ICP1 (D8)
  * - Timer 2 : OS Timer (1ms counter)
  *
- * Link OC0A (D6) and ICP1 (D8) together to read PWM
+ * Link OC0B (D5) and ICP1 (D8) together to read PWM
  * Result is displayed on serial output
- * Shoud read 244Hz and 0.5 duty cycle
+ * Shoud read 200Hz and 127 (50%) duty cycle
  */
 
 #define BUFFER_SZ 64
@@ -23,7 +23,8 @@ const __flash char STR_READY[] = "Ready!";
 const __flash char STR_ERROR[] = "Error %d";
 const __flash char STR_NO_INPUT[] = "No input";
 const __flash char STR_FREQUENCY[] = "Frequency = %uHz";
-const __flash char STR_DUTY_CYCLE[] = "Duty cycle = %.03f";
+const __flash char STR_DUTY_CYCLE[] = "Duty cycle = %u";
+const __flash char STR_ERROR_ICP_INIT[] = "INIT ICP FAILED";
 const __flash char STR_ERROR_TIMER_INIT[] = "INIT TIMER FAILED";
 const __flash char STR_ERROR_TIMER_START[] = "START TIMER FAILED";
 
@@ -34,21 +35,30 @@ void app_init()
 
     timer_config_t timer_cfg = {
         .mode = TIMER_MODE_FAST_PWM,
-        .output_mode_a = TIMER_OUTPUT_MODE_DEFAULT,
-        .prescaler = TIMER_PRESCALER_256,
-        .ocra = 127,
+        .output_mode_b = TIMER_OUTPUT_MODE_DEFAULT,
+        .oca_mode = TIMER_OCA_MODE_TOP,
+        .prescaler = TIMER_PRESCALER_1024,
+        .ocra = 77,
+        .ocrb = 38,
     };
 
     if (timer_init(TIMER_0, &timer_cfg) < 0)
     {
         serial_println_p(SERIAL_BUS_0, STR_ERROR_TIMER_INIT);
-    }
-    else if (timer_start(TIMER_0) < 0)
-    {
-        serial_println_p(SERIAL_BUS_0, STR_ERROR_TIMER_START);
+        HALT;
     }
 
-    icp_init(ICP1, ICP_PRESCALER_64, TRUE);
+    if (timer_start(TIMER_0) < 0)
+    {
+        serial_println_p(SERIAL_BUS_0, STR_ERROR_TIMER_START);
+        HALT;
+    }
+
+    if (icp_init(ICP1, ICP_PRESCALER_256, FALSE) < 0)
+    {
+        serial_println_p(SERIAL_BUS_0, STR_ERROR_ICP_INIT);
+        HALT;
+    }
 
     // Init tasks
     os_task_setup(TASK_MAIN, 1000, task_main, NULL_PTR);
@@ -65,16 +75,16 @@ int task_main(void* data)
     UNUSED(data);
 
     int ret = 0;
-    float dutyCycle = 0;
+    uint8_t duty_cycle = 0;
     uint16_t freq = 0;
 
     os_watchdog_trigger();
 
-    ret = icp_get_duty_cycle(ICP1, &dutyCycle);
+    ret = icp_get_duty_cycle(ICP1, &duty_cycle);
     switch(ret)
     {
         case ICP_OK:
-            snprintf_P(buffer, BUFFER_SZ, STR_DUTY_CYCLE, dutyCycle);
+            snprintf_P(buffer, BUFFER_SZ, STR_DUTY_CYCLE, duty_cycle);
             serial_println(SERIAL_BUS_0, buffer);
             break;
 
