078cfa5a9f203a33e0e28a530c6c3354f03dd928
nre <nre@2crsi.com>
2021-12-30 12:30:28 +0100

    [OS] implement get_free, get_total and get_used in stdlib malloc

--- src/os/src/mem/stdlib_alloc.c
+++ src/os/src/mem/stdlib_alloc.c
@@ -13,6 +13,9 @@
 static void* my_malloc(unsigned int sz);
 static void* my_calloc(unsigned int sz);
 static void my_free(void *ptr);
+static unsigned int my_get_free();
+static unsigned int my_get_total();
+static unsigned int my_get_used();
 
 /*
  * Private data
@@ -22,9 +25,9 @@ static struct malloc_drv_s drv = {
     .malloc = my_malloc,
     .calloc = my_calloc,
     .free = my_free,
-    .get_free = NULL_PTR,
-    .get_used = NULL_PTR,
-    .get_total = NULL_PTR
+    .get_free = my_get_free,
+    .get_used = my_get_used,
+    .get_total = my_get_total
 };
 
 /*
@@ -50,6 +53,27 @@ static void* my_calloc(unsigned int sz)
 static void my_free(void * ptr)
 {
     free(ptr);
+}
+
+static unsigned int my_get_used()
+{
+    extern int __heap_start, *__brkval;
+    return (unsigned int) __brkval - (unsigned int) &__heap_start;
+}
+
+static unsigned int my_get_free()
+{
+    extern int __heap_start, *__brkval;
+    uint8_t v, *stack_end = &v;
+    int *heap_end = __brkval == 0 ? &__heap_start : __brkval;
+    return (unsigned int) stack_end - (unsigned int) heap_end;
+}
+
+static unsigned int my_get_total()
+{
+    extern int __heap_start;
+    uint8_t v, *stack_end = &v;
+    return (unsigned int) stack_end - (unsigned int) &__heap_start;
 }
 
 #endif // OS_MALLOC == OS_MALLOC_GCC
