88186ce76b61b89a4d5feda6bb659c5c39c19678
nre <nre@2crsi.com>
2022-01-12 13:04:47 +0100

    [TFT-SHIELD] New module

---
+++ src/tft-shield/api/tft_shield.h
@@ -0,0 +1,32 @@
+#ifndef SRC_TFT_SHIELD_API_H_
+#define SRC_TFT_SHIELD_API_H_
+
+#include "types.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "st7735.h"
+
+typedef struct tft_shield_prv_s {
+    struct i2c_device_prv_s seesaw;
+    struct st7735_prv_s tft;
+} * tft_shield_t;
+
+typedef enum {
+    TFT_SHIELD_KEY_A,
+    TFT_SHIELD_KEY_B,
+    TFT_SHIELD_KEY_C,
+    TFT_SHIELD_KEY_JOY,
+    TFT_SHIELD_KEY_UP,
+    TFT_SHIELD_KEY_DOWN,
+    TFT_SHIELD_KEY_LEFT,
+    TFT_SHIELD_KEY_RIGHT,
+    TFT_SHIELD_NUMBER_OF_KEYS
+} tft_shield_key_t;
+
+int tft_shield_init(tft_shield_t self, i2c_bus_t i2c);
+int tft_shield_backlight(tft_shield_t self, uint8_t val);
+int tft_shield_read_keys(tft_shield_t self, tft_shield_key_t *key);
+
+st7735_t tft_shield_get_tft(tft_shield_t self);
+
+#endif /* SRC_TFT_SHIELD_API_H_ */
---
+++ src/tft-shield/src/tft_shield.c
@@ -0,0 +1,90 @@
+#include "tft_shield.h"
+#include "bits.h"
+#include "string.h"
+
+/*
+ * Private constants
+ */
+
+#define I2C_SEESAW       0x2E
+#define MODULE_PWM       0x08
+#define FUNCTION_PWM_VAL 0x01
+
+#define MODULE_GPIO          0x01
+#define FUNCTION_GPIO_DIRSET 0x02
+#define FUNCTION_GPIO_DIRCLR 0x03
+#define FUNCTION_GPIO_GPIO   0x04
+#define FUNCTION_GPIO_SET    0x05
+#define FUNCTION_GPIO_CLR    0x06
+#define FUNCTION_GPIO_PULSET 0x0B
+#define FUNCTION_GPIO_PULCLR 0x0C
+
+/*
+ * Public methods
+ */
+
+int tft_shield_init(tft_shield_t self, i2c_bus_t i2c)
+{
+    // Seesaw chip
+    i2c_device_init(&self->seesaw, i2c, I2C_SEESAW);
+
+    // TFT
+    st7735_init_device(&self->tft, D10, D8, 160, 128);
+
+    // Write 0xFFFFFFFF to GPIO registers
+    uint8_t cmd[5];
+    memset(&cmd, 0xFF, 5);
+
+    cmd[0] = FUNCTION_GPIO_DIRCLR;
+    i2c_device_write_bytes(&self->seesaw, MODULE_GPIO, cmd, 5);
+
+    cmd[0] = FUNCTION_GPIO_PULSET;
+    i2c_device_write_bytes(&self->seesaw, MODULE_GPIO, cmd, 5);
+
+    cmd[0] = FUNCTION_GPIO_SET;
+    i2c_device_write_bytes(&self->seesaw, MODULE_GPIO, cmd, 5);
+
+    return 0;
+}
+
+st7735_t tft_shield_get_tft(tft_shield_t self)
+{
+    return &self->tft;
+}
+
+int tft_shield_backlight(tft_shield_t self, uint8_t val)
+{
+    const uint8_t cmd[3] = { FUNCTION_PWM_VAL, 0, val };
+    if (i2c_device_write_bytes(&self->seesaw, MODULE_PWM, cmd, 3) != 3) return -1;
+    return 0;
+}
+
+int tft_shield_read_keys(tft_shield_t self, tft_shield_key_t *key)
+{
+    uint8_t buffer[4] = {MODULE_GPIO, FUNCTION_GPIO_GPIO, 0, 0};
+    if (i2c_device_transaction(&self->seesaw, buffer, 2, 4) != 4) return -1;
+
+    // 0xCF is read value when no key is pressed
+    switch( buffer[2] ^ 0xCF )
+    {
+        case 0: /* No key pressed */ break;
+        case BIT(0): *key = TFT_SHIELD_KEY_LEFT; return 0;
+        case BIT(1): *key = TFT_SHIELD_KEY_DOWN; return 0;
+        case BIT(2): *key = TFT_SHIELD_KEY_A;    return 0;
+        case BIT(3): *key = TFT_SHIELD_KEY_B;    return 0;
+        case BIT(6): *key = TFT_SHIELD_KEY_C;    return 0;
+        default: /* Multiple key press */ break;
+    }
+
+    // 0xEC is read value when no key is pressed
+    switch( buffer[3] ^ 0xEC )
+    {
+        case 0: /* No key pressed */ break;
+        case BIT(5): *key = TFT_SHIELD_KEY_RIGHT; return 0;
+        case BIT(6): *key = TFT_SHIELD_KEY_UP;    return 0;
+        case BIT(7): *key = TFT_SHIELD_KEY_JOY;   return 0;
+        default: /* Multiple key press */ break;
+    }
+
+    return -2;
+}
