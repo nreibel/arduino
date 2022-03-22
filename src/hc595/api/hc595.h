aa628e1bfe1e534cf43d1a34116b4d00537a4fc4
Nicolas Reibel <nicolas.reibel@gmail.com>
2022-03-22 16:57:24 +0100

    [HC595] Update API + code

--- src/hc595/api/hc595.h
+++ src/hc595/api/hc595.h
@@ -12,15 +12,15 @@ typedef struct hc595_prv_s {
 } * hc595_t;
 
 #if OS_MALLOC
-hc595_t hc595_create(gpio_t serial, gpio_t clock, gpio_t latch);
+hc595_t hc595_create();
 void hc595_destroy(hc595_t self);
 #endif // OS_MALLOC
 
 void hc595_init(hc595_t self, gpio_t serial, gpio_t clock, gpio_t latch);
-void hc595_write(hc595_t self, buffer_t data, int len);
+int hc595_write(hc595_t self, void * data, int len);
 
-void hc595_write_byte(hc595_t self, uint8_t val);
-void hc595_write_word(hc595_t self, uint16_t val);
-void hc595_write_dword(hc595_t self, uint32_t val);
+int hc595_write_byte(hc595_t self, uint8_t val);
+int hc595_write_word(hc595_t self, uint16_t val);
+int hc595_write_dword(hc595_t self, uint32_t val);
 
 #endif /* SRC_hc595_API_H_ */
--- src/hc595/src/hc595.c
+++ src/hc595/src/hc595.c
@@ -3,21 +3,19 @@
 #include "gpio.h"
 #include "os.h"
 
-#ifndef HC595_EDGE_DELAY
-#define HC595_EDGE_DELAY 1
-#endif // HC595_EDGE_DELAY
+#define HC595_EDGE_DELAY 1 // in µs
 
 #if OS_MALLOC
-hc595_t hc595_create(gpio_t serial, gpio_t clock, gpio_t latch)
+hc595_t hc595_create()
 {
     hc595_t instance = os_malloc(sizeof(*instance));
-    if (instance != NULL_PTR) hc595_init(instance, serial, clock, latch);
     return instance;
 }
 
 void hc595_destroy(hc595_t self)
 {
-    os_free(self);
+    if (self != NULL_PTR)
+        os_free(self);
 }
 #endif // OS_MALLOC
 
@@ -32,39 +30,43 @@ void hc595_init(hc595_t self, gpio_t serial, gpio_t clock, gpio_t latch)
     self->latch = latch;
 }
 
-void hc595_write(hc595_t self, buffer_t data, int len)
+int hc595_write(hc595_t self, void * data, int len)
 {
-    for (int i = len ; i > 0 ; i--)
+    uint8_t * bytes = data;
+
+    for (int i = len-1 ; i >= 0 ; i--)
     {
-        uint8_t val = READ_PU8(data+i-1);
         for (int j = 7 ; j >= 0 ; j--)
         {
-            bool st = IS_SET_BIT(val, j);
+            bool st = IS_SET_BIT(bytes[i], j);
+
             if (st) gpio_set(self->serial);
             else gpio_reset(self->serial);
 
             gpio_set(self->clock);
-            os_wait(HC595_EDGE_DELAY);
+            os_wait_us(HC595_EDGE_DELAY);
             gpio_reset(self->clock);
         }
     }
 
     gpio_set(self->latch);
-    os_wait(HC595_EDGE_DELAY);
+    os_wait_us(HC595_EDGE_DELAY);
     gpio_reset(self->latch);
+
+    return len;
 }
 
-void hc595_write_byte(hc595_t self, uint8_t val)
+int hc595_write_byte(hc595_t self, uint8_t val)
 {
-    hc595_write(self, &val, 1);
+    return hc595_write(self, &val, 1);
 }
 
-void hc595_write_word(hc595_t self, uint16_t val)
+int hc595_write_word(hc595_t self, uint16_t val)
 {
-    hc595_write(self, &val, 2);
+    return hc595_write(self, &val, 2);
 }
 
-void hc595_write_dword(hc595_t self, uint32_t val)
+int hc595_write_dword(hc595_t self, uint32_t val)
 {
-    hc595_write(self, &val, 4);
+    return hc595_write(self, &val, 4);
 }
