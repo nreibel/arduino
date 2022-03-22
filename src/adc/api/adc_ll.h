538256c84abeaf0f416f7cc1eac0709a9a02a7f4
Nicolas Reibel <nicolas.reibel@gmail.com>
2022-03-22 11:04:58 +0100

    [ADC] Update ADC LL code and API

--- src/adc/api/adc_ll.h
+++ src/adc/api/adc_ll.h
@@ -7,11 +7,6 @@
 #define ADC_MAX_VALUE  ((1U << (ADC_RESOLUTION)) - 1)
 
 enum {
-    ADC_LL_OK,
-    ADC_LL_ERROR
-};
-
-enum {
     ADC_LL_VREF_AREF        = 0x0,
     ADC_LL_VREF_AVCC        = 0x1,
     ADC_LL_VREF_RESERVED    = 0x2,
@@ -42,6 +37,16 @@ typedef union {
 
 typedef union {
     struct {
+        uint8_t adts        : 3;
+        uint8_t __reserved1 : 3;
+        uint8_t acme        : 1;
+        uint8_t __reserved2 : 1;
+    } bits;
+    uint8_t reg;
+} adcsrb_t;
+
+typedef union {
+    struct {
         uint8_t mux        : 4;
         uint8_t __reserved : 1;
         uint8_t adlar      : 1;
@@ -52,28 +57,29 @@ typedef union {
 
 typedef struct {
     volatile union {
-        uint8_t bytes[2];
-        uint16_t u16;
+        uint8_t b[2];
+        uint16_t w;
     } adc;
-    volatile adcsra_t  adcsra;
-    volatile uint8_t   adcsrb;
-    volatile admux_t   admux;
+    volatile adcsra_t adcsra;
+    volatile adcsrb_t  adcsrb;
+    volatile admux_t  admux;
 } * adc_t;
 
 #define ADC0 TYPECAST(0x78, const adc_t)
 
 extern void adc_interrupt(adc_t adc, uint16_t value);
 
-int adc_ll_init(adc_t adc);
-int adc_ll_set_enabled(adc_t adc, bool enabled);
-int adc_ll_set_interrupts_enabled(adc_t adc, bool enabled);
-int adc_ll_set_prescaler(adc_t adc, uint8_t pscl);
-int adc_ll_set_vref(adc_t adc, uint8_t vref);
-int adc_ll_set_left_aligned(adc_t adc, bool left_aligned);
-int adc_ll_select_channel(adc_t adc, uint8_t channel);
-int adc_ll_start_conversion(adc_t adc);
-int adc_ll_wait_conversion(adc_t adc);
-int adc_ll_read_word(adc_t adc, uint16_t * data);
-int adc_ll_read_byte(adc_t adc, uint8_t * data);
+void adc_ll_init(adc_t adc);
+void adc_ll_set_enabled(adc_t adc, bool enabled);
+void adc_ll_set_interrupts_enabled(adc_t adc, bool enabled);
+void adc_ll_set_prescaler(adc_t adc, uint8_t pscl);
+void adc_ll_set_vref(adc_t adc, uint8_t vref);
+void adc_ll_set_left_aligned(adc_t adc, bool left_aligned);
+void adc_ll_select_channel(adc_t adc, uint8_t channel);
+void adc_ll_start_conversion(adc_t adc);
+void adc_ll_wait_conversion(adc_t adc);
+
+uint8_t adc_ll_read_byte(adc_t adc);
+uint16_t adc_ll_read_word(adc_t adc);
 
 #endif /* ADC_LL_H__ */
--- src/adc/src/adc_ll.c
+++ src/adc/src/adc_ll.c
@@ -17,79 +17,68 @@ void adc_interrupt(adc_t adc, uint16_t value)
 
 ISR(ADC_vect)
 {
-    adc_interrupt(ADC0, ADC0->adc.u16);
+    adc_interrupt(ADC0, ADC0->adc.w);
 }
 
 /*
  * Exported functions
  */
 
-int adc_ll_init(adc_t adc)
+void adc_ll_init(adc_t adc)
 {
     power_adc_enable();
+
     adc->adcsra.reg = 0;
-    adc->adcsrb = 0;
-    return ADC_LL_OK;
+    adc->adcsra.reg = 0;
+    adc->admux.reg = 0;
 }
 
-int adc_ll_set_enabled(adc_t adc, bool enabled)
+void adc_ll_set_enabled(adc_t adc, bool enabled)
 {
     adc->adcsra.bits.aden = enabled ? 1 : 0;
-    return ADC_LL_OK;
 }
 
-int adc_ll_set_interrupts_enabled(adc_t adc, bool enabled)
+void adc_ll_set_interrupts_enabled(adc_t adc, bool enabled)
 {
     adc->adcsra.bits.adie = enabled ? 1 : 0;
-    return ADC_LL_OK;
 }
 
-int adc_ll_set_prescaler(adc_t adc, uint8_t pscl)
+void adc_ll_set_prescaler(adc_t adc, uint8_t pscl)
 {
     adc->adcsra.bits.adps = pscl;
-    return ADC_LL_OK;
 }
 
-int adc_ll_set_vref(adc_t adc, uint8_t vref)
+void adc_ll_set_vref(adc_t adc, uint8_t vref)
 {
     adc->admux.bits.refs = vref;
-    return ADC_LL_OK;
 }
 
-int adc_ll_set_left_aligned(adc_t adc, bool left_aligned)
+void adc_ll_set_left_aligned(adc_t adc, bool left_aligned)
 {
     adc->admux.bits.adlar = left_aligned ? 1 : 0;
-    return ADC_LL_OK;
 }
 
-int adc_ll_select_channel(adc_t adc, uint8_t channel)
+void adc_ll_select_channel(adc_t adc, uint8_t channel)
 {
     adc->admux.bits.mux = channel;
-    return ADC_LL_OK;
 }
 
-int adc_ll_wait_conversion(adc_t adc)
+void adc_ll_wait_conversion(adc_t adc)
 {
     while(adc->adcsra.bits.adsc);
-    return ADC_LL_OK;
 }
 
-int adc_ll_start_conversion(adc_t adc)
+void adc_ll_start_conversion(adc_t adc)
 {
     adc->adcsra.bits.adsc = 1;
-    return ADC_LL_OK;
 }
 
-int adc_ll_read_word(adc_t adc, uint16_t * data)
+uint16_t adc_ll_read_word(adc_t adc)
 {
-    *data = adc->adc.u16;
-    return ADC_LL_OK;
+    return adc->adc.w;
 }
 
-int adc_ll_read_byte(adc_t adc, uint8_t * data)
+uint8_t adc_ll_read_byte(adc_t adc)
 {
-    *data = adc->adc.bytes[1];
-    return ADC_LL_OK;
+    return adc->adc.b[1];
 }
-
-
