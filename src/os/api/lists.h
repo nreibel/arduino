a06a0828e5c98c4486b4a2fe578faa06f3756bcc
nre <nre@2crsi.com>
2023-01-03 11:36:08 +0100

    [OS] update lists

--- src/os/api/lists.h
+++ src/os/api/lists.h
@@ -4,14 +4,17 @@
 #include "types.h"
 #include "os_cfg.h"
 
-#if OS_MALLOC
+#if OS_MALLOC != OS_MALLOC_NONE
 
 typedef struct linked_list_prv_s * linked_list_t;
 
-linked_list_t linked_list_create();
-linked_list_t linked_list_append(linked_list_t list, void *data);
-void* linked_list_get(linked_list_t list, unsigned int index);
-unsigned int linked_list_size(linked_list_t list);
+linked_list_t   linked_list_create  (void);
+linked_list_t   linked_list_push    (linked_list_t list, void * data);
+void *          linked_list_get     (linked_list_t list, unsigned int index);
+void *          linked_list_pop     (linked_list_t list);
+void *          linked_list_peek    (linked_list_t list);
+unsigned int    linked_list_size    (linked_list_t list);
+bool            linked_list_empty   (linked_list_t list);
 
 #endif // OS_MALLOC
 
--- src/os/src/lists.c
+++ src/os/src/lists.c
@@ -6,62 +6,80 @@
  * This implementation is for educationnal purposes only
  */
 
-#if OS_MALLOC
+#if OS_MALLOC != OS_MALLOC_NONE
 
 struct linked_list_prv_s {
-    void *data;
+    void * data;
     linked_list_t next;
 };
 
-linked_list_t linked_list_create()
+linked_list_t linked_list_create(void)
 {
-    return os_calloc(sizeof(struct linked_list_prv_s));
+    linked_list_t self = os_calloc(sizeof(*self));
+    return self;
 }
 
-linked_list_t linked_list_append(linked_list_t self, void *data)
+linked_list_t linked_list_push(linked_list_t self, void * data)
 {
-    if (self == NULL_PTR)
+    if (self->next != NULL_PTR)
+        return linked_list_push(self->next, data);
+
+    const linked_list_t item = linked_list_create();
+
+    if (item)
     {
-        return NULL_PTR;
-    }
-    else if (self->data == NULL_PTR)
-    {
-        self->data = data;
+        item->data = data;
+        item->next = NULL_PTR;
+        self->next = item;
         return self;
     }
-    else if (self->next != NULL_PTR)
-    {
-        return linked_list_append(self->next, data);
-    }
-    else
-    {
-        linked_list_t item = linked_list_create();
 
-        if (item != NULL_PTR)
-        {
-            item->data = data;
-            self->next = item;
-        }
-
-        return item;
-    }
+    return NULL_PTR;
 }
 
-void* linked_list_get(linked_list_t self, unsigned int index)
+void * linked_list_peek(linked_list_t self)
 {
-    if (self == NULL_PTR) return NULL_PTR;
-    else if (index == 0) return self->data;
-    else if (self->next != NULL_PTR) return linked_list_get(self->next, index-1);
-    else return NULL_PTR;
+    if (self->next)
+        return self->next->data;
+
+    return NULL_PTR;
+}
+
+void * linked_list_pop(linked_list_t self)
+{
+    void * ret = NULL_PTR;
+
+    if (self->next)
+    {
+        ret = self->next->data;
+        self->next = self->next->next;
+    }
+
+    return ret;
+}
+
+void * linked_list_get(linked_list_t self, unsigned int index)
+{
+    if (!self->next)
+        return NULL_PTR;
+
+    if (index > 0)
+        return linked_list_get(self->next, index-1);
+
+    return self->next->data;
 }
 
 unsigned int linked_list_size(linked_list_t self)
 {
-    if (self == NULL_PTR) return 0;
-    else if (self->data == NULL_PTR) return 0;
-    else if (self->next != NULL_PTR) return 1 + linked_list_size(self->next);
-    else return 1;
+    if (!self->next)
+        return 0;
 
+    return 1 + linked_list_size(self->next);
+}
+
+bool linked_list_empty(linked_list_t self)
+{
+    return self->next == NULL_PTR;
 }
 
 #endif // OS_MALLOC
